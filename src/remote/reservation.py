59006f21e7481aa5dbaa9571b8980c5f781f72a9
radon <sebastiaanalva@gmail.com>
2020-11-05 13:13:52 +0100

    Preparing for reservation push

--- src/remote/reservation.py
+++ src/remote/reservation.py
@@ -2,7 +2,4 @@ from config.meta import cfg_meta_instance as metacfg
 
 def get_reserved_nodes(infiniband=False):
     nodes = subprocess.check_output("preserve -llist | grep "+metacfg.ssh.ssh_user_name+" | awk -F'\\t' '{ print $NF }'").check_output('utf-8').strip().split('\n')[-1].split()
-    if infiniband:
-        return [ip.node_to_infiniband_ip(int(x[4:])) for x in self.nodes]
-    else:
-        return nodes
+    return [ip.node_to_infiniband_ip(int(x[4:])) for x in nodes] if infiniband else nodes
